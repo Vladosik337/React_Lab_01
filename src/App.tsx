c65ec953f35a5ace7bf24b930f81fee5d1f15e62
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-17 15:46:35 +0300

    create component Keyboard and change title in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './styles/style.scss';
+import Keyboard from "./components/Keyboard.tsx";
+
 
 function App() {
+    const handleKeyPress = (key) => {
+        console.log('Натиснута клавіша:', key);
+    };
+
   return (
     <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1>Віртуальна клавіатура</h1>
+        <Keyboard onKeyPress={handleKeyPress}/>
     </div>
   );
 }
